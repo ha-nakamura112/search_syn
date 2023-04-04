2688687399fecd9ee7679e0e110441c0124d19fe
ha-nakamura112 <nh.3213.b.b@gmail.com>
2023-04-05 05:49:23 +0900

    eivironmental valuable and fix code

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,34 +1,61 @@
-const { Configuration, OpenAIApi } = require("openai");
+// pages/api/synonyms/[word].js
+import dotenv from 'dotenv';
+import { OpenAIApi, Configuration } from 'openai';
 
-const config = new Configuration({
-	apiKey: "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",
-});
+dotenv.config();
 
-const openai = new OpenAIApi(config);
+const API_KEY = process.env.OPEN_CHAT_KEY;
 
-const runPrompt = async () => {
-	const prompt = `
-        write me a joke about a cat and a bowl of pasta. Return response in the following parsable JSON format:
+export default async function handler(req, res) {
+  const first  = req.body.first;
+  const word  = req.body.word;
+	const config = new Configuration({
+    apiKey: API_KEY
+  });
+  
+  const openai = new OpenAIApi(config);
 
-        {
-            "Q": "question",
-            "A": "answer"
-        }
+  try {
+					const prompt = `
+					Give me some words which is a synonyms with 
+					"${first}${word}" in belows format which can be convert to Json. Don't put answer before your answer and meaning should be only two.
+					Eachmeaning should be more than 10 words and not be same as ${word}
+				[
+					{"index":1,
+				"word":"Vassily",
+				"meaning":[
+					"first meaning",
+					"second meaning"
+				]
+			},
+				{
+					"index":2,
+				"word":"Hesther",
+				"meaning":[
+					"first meaning",
+					"second meaning"
+				]
+			}
+			]
+			`;
+			const response = await openai.createCompletion({
+				model: "text-davinci-003",
+				prompt: prompt,
+				max_tokens: 2048,
+				temperature: 1,
+			});
 
-    `;
 
-	const response = await openai.createCompletion({
-		model: "text-davinci-003",
-		prompt: prompt,
-		max_tokens: 2048,
-		temperature: 1,
-	});
+    if (!response) {
+      return res.status(404).json({ message: 'No synonyms found.' });
+    }
 
-	const parsableJSONresponse = response.data.choices[0].text;
-	const parsedResponse = JSON.parse(parsableJSONresponse);
+		const parsableJSONresponse = response.data.choices[0].text;
+    console.log(parsableJSONresponse)
+		res.status(200).json(JSON.parse(parsableJSONresponse));
 
-	console.log("Question: ", parsedResponse.Q);
-	console.log("Answer: ", parsedResponse.A);
-};
-
-runPrompt();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error });
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,26 @@
-
 import React, { useState } from 'react';
 
-import { Configuration,OpenAIApi } from "openai";
-
-const config = new Configuration({
-	apiKey: "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",
-});
-
-const openai = new OpenAIApi(config);
-
 function Home() {
+  const server = 'http://localhost:3000/';
+  
   const [ userInput, setUserInput] = useState('');
-  const [ meanings, setMeanings ] = useState(new Map());
   const [ parsedResponse, setparsedResponse ] = useState('');
+
   const runPrompt = async (e) => {
     e.preventDefault();
-    
-    const prompt = `
-          Give me some words which is a synonyms with 
-          "${e.target[0].value} ${userInput}" in belows format which can be convert to Json. Don't put answer before your answer and meaning should be only two.
-          Eachmeaning should be more than 10 words and not be same as ${userInput}
-        [
-          {"index":1,
-        "word":"Vassily",
-        "meaning":[
-          "first meaning",
-          "second meaning"
-        ]
-      },
-
-        {
-          "index":2,
-        "word":"Hesther",
-        "meaning":[
-          "first meaning",
-          "second meaning"
-        ]
-      }
-    ]
-      `;
-  
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 2048,
-      temperature: 1,
+    const formData = new FormData();
+    formData.append('first', e.target[0].value);
+    formData.append('word', userInput);
+    const response = await fetch(`${server}api/hello`, {
+      method: 'POST',
+      body: formData,
     });
-  
-    const parsableJSONresponse = response.data.choices[0].text;
-    console.log(parsableJSONresponse)
-    setparsedResponse(JSON.parse(parsableJSONresponse))
-  
+    const data = await response.json();
+    console.log(data);
+    setparsedResponse(data);
   };
+  
+
   return (
     <div>
       <form onSubmit={(e)=>runPrompt(e)}>
@@ -71,13 +40,6 @@ function Home() {
                 <h3>{res.word}</h3>
                 <div>{res.meaning[0]}</div>
                 <div>{res.meaning[1]}</div>
-                {/* { res.meaning.map((mean,idx)=> {
-                  <ul key={idx}>
-                    <li>{mean}</li>
-                    <li>{mean[0]}</li>
-                    <li>{mean[1]}</li>
-                  </ul>
-                }) } */}
               </div>
             )
           })
